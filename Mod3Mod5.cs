3e5969baca04c1e67ec29ed5ff5b574b34f84493
belkiOlur <114578271+belkiOlur@users.noreply.github.com>
2022-10-11 10:15:32 +0300

    Add files via upload

---
+++ Mod3Mod5.cs
@@ -0,0 +1,11 @@
+﻿int toplam = 0;
+
+for (int i = 0; i<1000; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        toplam +=  i;
+    }
+    
+}
+Console.Write("1000'e kadar 3 veya 5'in katları toplamı = "+toplam);
